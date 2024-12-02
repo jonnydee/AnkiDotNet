902a0f6c46dda669845e690a162bafb37fc0b73b
Johann Duscher <jonny.dee@posteo.net>
2024-12-02 19:48:18 +0100

    feat(Infrastructure): Add `InMemoryCollectionServiceFactory` implementation.

---
+++ src/AnkiNet/Infrastructure/InMemoryCollectionServiceFactory.cs
@@ -0,0 +1,19 @@
+﻿using AnkiNet.DomainModel;
+using AnkiNet.Infrastructure.Persistence.Repositories;
+
+namespace AnkiNet.Infrastructure;
+
+public sealed class InMemoryCollectionServiceFactory
+    : ICollectionServiceFactory
+{
+    public Task<ICollectionService> CreateCollectionServiceAsync()
+    {
+        ICollectionService instance = new CollectionService(
+            new InMemoryDeckRepository(),
+            new InMemoryNoteRepository(),
+            new InMemoryNoteTypeRepository(),
+            new InMemoryCollectionRepository());
+
+        return Task.FromResult(instance);
+    }
+}
